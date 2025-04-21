0b8dcb9515232079bbacf4f140f8a311a81e7b42
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2025-04-21 13:18:44 +0530

    ✨ Updating state

--- React Basics/my-app/src/App.jsx
+++ React Basics/my-app/src/App.jsx
@@ -4,12 +4,43 @@ import Message from "./Message";
 
 class App extends React.Component {
   render() {
-    let msgOne = { msg: "Hey !", from: "John", to: "Carter" };
-    // let msgTwo = { msg: "Hello !", from: "Peter", to: "Parker" };
-    // let msgThree = { msg: "Bye !", from: "Mick", to: "Harington" };
+    let msgs = [
+      {
+        msg: "Hey !",
+        from: "John",
+        to: "Carter",
+        likes: 100,
+        imageUrl:
+          "https://images.squarespace-cdn.com/content/v1/5e949a92e17d55230cd1d44f/65963c6a-a64d-44ba-bef1-0d15bc5f25dc/Hello201x1.png",
+      },
+      {
+        msg: "Hello !",
+        from: "Peter",
+        to: "Parker",
+        likes: 200,
+        imageUrl:
+          "https://images.squarespace-cdn.com/content/v1/5e949a92e17d55230cd1d44f/65963c6a-a64d-44ba-bef1-0d15bc5f25dc/Hello201x1.png",
+      },
+      {
+        msg: "Bye !",
+        from: "Mick",
+        likes: 300,
+        to: "Harington",
+        imageUrl:
+          "https://images.squarespace-cdn.com/content/v1/5e949a92e17d55230cd1d44f/65963c6a-a64d-44ba-bef1-0d15bc5f25dc/Hello201x1.png",
+      },
+    ];
     return (
       <div style={{ margin: "20px" }}>
-        <Message messageDetails={msgOne} />
+        {msgs.map(msg => {
+          return <Message messageDetails={msg} />;
+        })}
+
+        {msgs.length > 0 ? (
+          <h2>{msgs.length} messages </h2>
+        ) : (
+          <h2>No Messages</h2>
+        )}
       </div>
     );
   }
--- React Basics/my-app/src/Message.jsx
+++ React Basics/my-app/src/Message.jsx
@@ -1,12 +1,32 @@
 import React from "react";
 
 class Message extends React.Component {
+  state = { currLikes: 700 };
+  IncrementLikes() {
+    console.log("IncrementLikes");
+    // this.props.messageDetails.likes++; // props are readonly
+    // this.state.currLikes++; // state is immutable
+    this.setState({ currLikes: this.state.currLikes + 1 });
+  }
   render() {
     return (
-      <div>
+      <div
+        style={{ border: "1px solid black", margin: "10px", padding: "10px" }}
+      >
+        <img
+          src={this.props.messageDetails.imageUrl}
+          alt=""
+          height={"100px"}
+          width={"150px"}
+        />
         <h2>{this.props.messageDetails.msg}</h2>
         <p>From : {this.props.messageDetails.from}</p>
         <p>To : {this.props.messageDetails.to}</p>
+        <button onClick={() => this.IncrementLikes()}>
+          {" "}
+          {this.state.currLikes} Likes
+          {/* {this.props.messageDetails.likes} */}
+        </button>
       </div>
     );
   }
