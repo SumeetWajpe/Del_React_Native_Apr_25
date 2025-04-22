710f3ae22fb2002dc0cafcb40df4aec923fa5fe4
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2025-04-22 11:54:35 +0530

    ✨ using setCount to update the count

--- React Basics/my-app/src/functional/counter.jsx
+++ React Basics/my-app/src/functional/counter.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
 
 const Counter = () => {
-  const [count] = useState(100); // state hook - returns an array (stateVar,setStateVar) - initially set to 100
+  const [count, setCount] = useState(100); // state hook - returns an array (stateVar,setStateVar) - initially set to 100
   return (
     <div>
       <strong>Counter : {count}</strong>
+      <button onClick={() => setCount(count + 1)}>++</button>
     </div>
   );
 };
