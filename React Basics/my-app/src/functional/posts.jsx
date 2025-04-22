855535f4521d4b6fe5600e9883b9ef64c881e4fa
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2025-04-22 13:03:26 +0530

    ✨ using useEffect with Dependency list

--- React Basics/my-app/src/functional/posts.jsx
+++ React Basics/my-app/src/functional/posts.jsx
@@ -1,14 +1,20 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 const Posts = () => {
+  const [posts, setPosts] = useState([]);
   useEffect(() => {
     axios.get("https://jsonplaceholder.typicode.com/posts").then(response => {
-      console.log(response.data);
+      setPosts(response.data);
     });
-  });
+  }, []);
   return (
     <div>
       <h1>All Posts</h1>
+      <ul>
+        {posts.map(post => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
     </div>
   );
 };
