eb83aeec6926da14cc951c05a094d7281f6a3997
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2025-04-29 12:05:23 +0530

    ✨ Token verification (Node App)

--- Node_App_JWT/app.js
+++ Node_App_JWT/app.js
@@ -8,10 +8,28 @@ app.post("/login", (req, res) => {
     { name: "admin" },
     secret_key,
     { expiresIn: "2 Days" },
-    (er, token) => {
+    (err, token) => {
+      if (err) console.log(err);
       res.json({ token });
     },
   );
+});
+
+app.post("/verify", (req, res) => {
+  let value = req.headers.authorization;
+  let token = value.split(" ")[1];
+  if (token) {
+    jwt.verify(token, secret_key, (err, decodedToken) => {
+      if (!err) {
+        console.log(decodedToken);
+        res.json({ msg: "Token Verified !" });
+      } else {
+        res.status(401).send({ msg: "Invalid Token" });
+      }
+    });
+  } else {
+    res.status(500).send("Token not found !");
+  }
 });
 
 app.listen(4000, () => console.log("Server running @ port 4000 !"));
